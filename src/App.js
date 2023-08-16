5fd33153d36c3b7822ad17ad4e3c39f792762f29
mdyasin111 <mdyasin7636@gmail.com>
2023-08-16 11:31:18 +0600

    exploring styled components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import { StyledButton } from './components/styled/Button';
 
 function App() {
    return (
       <div className='App'>
          <div className='button_box'>
-            <button>Live</button>
-            <button className='primary'>Github</button>
+            <StyledButton>Live</StyledButton>
+            <StyledButton variant='primary' >Github</StyledButton>
          </div>
       </div>
    );
---
+++ src/components/styled/Button.js
@@ -0,0 +1,16 @@
+import { styled } from "styled-components";
+
+export const StyledButton = styled.button`
+  display: inline-block;
+  border-radius: 3px;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
+  width: 11rem;
+  background: transparent;
+  color: white;
+  border: 2px solid white;
+  cursor: pointer;
+  background-color: ${({variant}) =>
+    variant === "primary" ? "#ffffff" : "transparent"};
+  color: ${(props) => (props.variant === "primary" ? "#000000" : "#ffffff")};
+`;
