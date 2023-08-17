ad97ea26bde066746beb18a4715a8ea34a00629a
mdyasin111 <mdyasin7636@gmail.com>
2023-08-17 22:34:03 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import "./App.css";
-import { StyledButton, StyledButtonWithHover } from "./components/styled/Button";
-import { StyledDiv } from "./components/styled/StyledDiv";
+// import { StyledButton, StyledButtonWithHover } from "./components/styled/Button";
+// import { StyledDiv } from "./components/styled/StyledDiv";
 // import { H1 } from "./components/styled/H1";
 
 function App() {
   return (
-    <div className="App">
-      <div className="button_box">
-        <StyledButton>Live</StyledButton>
-        <StyledButton variant="primary">Github</StyledButton>
-        {/* <H1 color="red">Demo 1</H1>
-        <H1 color="green">Demo 2</H1>
-        <H1 color="yellow">Demo 3</H1> */}
-      </div>
-      <StyledDiv>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, nisi!
-          <a href="b">Inside p tag</a>
-        </p>
-        <a href="a">Link</a>
-      </StyledDiv>
-      <StyledButtonWithHover>Hover Me</StyledButtonWithHover>
+    // <div className="App">
+    //   <div className="button_box">
+    //     <StyledButton>Live</StyledButton>
+    //     <StyledButton variant="primary">Github</StyledButton>
+    //     <H1 color="red">Demo 1</H1>
+    //     <H1 color="green">Demo 2</H1>
+    //     <H1 color="yellow">Demo 3</H1>
+    //   </div>
+    //   <StyledDiv>
+    //     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, nisi!
+    //       <a href="b">Inside p tag</a>
+    //     </p>
+    //     <a href="a">Link</a>
+    //   </StyledDiv>
+    //   <StyledButtonWithHover>Hover Me</StyledButtonWithHover>
+    // </div>
+    <div>
+      
     </div>
   );
 }
--- src/components/styled/H1.js
+++ src/components/styled/H1.js
@@ -2,4 +2,9 @@ import { styled } from "styled-components";
 
 export const H1 = styled.h1`
   color: ${({ color }) => color};
+
+  &:hover {
+    background-color: white;
+    color: black;
+  }
 `;
